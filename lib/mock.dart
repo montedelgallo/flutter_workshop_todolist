01a1bd5259fb4319efb763d69cd33d4176800153
Dmaccagno <davide.macc@gmail.com>
2018-10-02 00:32:07 +0200

    adding some interactivity with mock data

--- lib/mock.dart
+++ lib/mock.dart
@@ -2,26 +2,28 @@ import 'package:flutter/material.dart';
 
 
 class Todo {
+  final int id;
   final String title;
   final String description;
-  const Todo({this.title,this.description});
+  final Function onDone;
+  const Todo({this.id,this.title,this.description,this.onDone});
 
 }
 
 class TodoCard extends Card {
 
-  TodoCard(Todo contact) :
+  TodoCard(Todo todo) :
         super(
           child: new Column(children: <Widget>[
-            new ListTile(title : new Text(contact.title),
-                subtitle: new Text(contact.description),
+            new ListTile(title : new Text(todo.title),
+                subtitle: new Text(todo.description),
                 leading: const Icon(Icons.album)),
             new ButtonTheme.bar( // make buttons use the appropriate styles for cards
               child: new ButtonBar(
                 children: <Widget>[
                   new FlatButton(
                     child: const Text('Mark as Done'),
-                    onPressed:,
+                    onPressed:todo.onDone,
                   ),
                 ],
               ),
@@ -34,7 +36,17 @@ List<Todo> getCards(int dimension) {
   List<Todo> todos = [];
 
   for (int i = 0; i < dimension; i++) {
-    todos.add(new Todo(title:"Todos $i", description:"Description $i"));
+
+    todos.add(
+        new Todo(
+            id:i,
+            title:"Todos $i",
+            description:"Description $i",
+            onDone: (){
+
+              print("call api for id:$i");
+
+            }));
   }
 
   return todos;
