5c947d415862a8765f1b4a30e70826a8887dfecd
Dmaccagno <davide.macc@gmail.com>
2018-10-03 09:05:50 +0200

    Adding some errors for teaching purposes

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,114 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+
+    List<String> titles = ["Home","AddTodo"];
+    List<Todo> _todoItems = [];
+    GlobalKey<_MyHomePageState> mainKey = new GlobalKey();
+    List<GlobalKey> keys =[mainKey];
+    AppState state = new AppState(titles: titles,list: _todoItems,keys: keys);
+
+
     return new MaterialApp(
       title: 'Workshop TodoList',
+      initialRoute: '/',
+      routes: {
+        // When we navigate to the "/" route, build the MyHomePage Widget
+        '/': (context) => MyHomePage(state: state)
+      },
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Workshop TodoList'),
+    );
+  }
+}
+
+class AddTodo extends StatefulWidget{
+
+  AddTodo({Key key, this.state}) : super(key: key);
+
+  final AppState state;
+
+  _AddTodo createState() => _AddTodo();
+
+}
+
+class _AddTodo extends State<AddTodo> {
+
+
+  Widget _build(BuildContext context) {
+    return new Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(),
+            TextField(),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text("Done"),
+                  onPressed: (){
+
+                      Todo t = new Todo(id:widget.state.list.length,title:"test",description: "test",isCompleted: false);
+                      MyHomePage.of(context).addTodoItem(t);
+                  },
+                ),
+                FlatButton(
+                  child: Text("Back"),
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        "/", (Route<dynamic> route) => false);
+                  },
+                )
+              ],
+            )
+          ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Add Todo"),
+        ),
+        body:
+        _build(context) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
+  static _MyHomePageState of(BuildContext context) => context.ancestorStateOfType(const TypeMatcher<_MyHomePageState>());
+
+  MyHomePage({Key key, this.state}) : super(key: key);
+
+  final AppState state;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Todo> _todoItems = [];
+
+
+
+  List<Todo> _local;
+
+  @override
+  void initState() {
+    super.initState();
+    _local=widget.state.list;
+
+  }
 
   void markAsDone(Todo todo) {
     setState(() {
-      _todoItems[todo.id] = new Todo(
+      _local[todo.id] = new Todo(
           id: todo.id,
           title: todo.title,
           description: todo.description,
@@ -83,19 +166,23 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _addTodoItem() {
-    setState(() {
-      int index = _todoItems.length;
-      _todoItems
-          .add(new Todo(id: index, title: "task", description: "description",isCompleted: false));
-    });
+
+
+  void addTodoItem(Todo todo) {
+
+     setState(() {
+
+       widget.state.list.add(todo);
+
+     });
+
   }
 
   Widget _buildTodoList() {
     return new ListView.builder(
-      itemCount: _todoItems.length,
+      itemCount: _local.length,
       itemBuilder: (context, index) {
-        return _buildTodoItem(_todoItems[index]);
+        return _buildTodoItem(widget.state.list[index]);
       },
     );
   }
@@ -104,14 +191,19 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.title),
+        title: new Text(widget.state.titles[0]),
       ),
       body: _buildTodoList(),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _addTodoItem,
+        onPressed: (){Navigator.of(context)
+            .pushAndRemoveUntil(new MaterialPageRoute(
+            builder: (BuildContext context)=>new AddTodo(state:widget.state))
+            , (Route<dynamic> route) => false);},
         tooltip: 'Refresh',
-        child: new Icon(Icons.refresh),
+        child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
 }
--- lib/mock.dart
+++ lib/mock.dart
@@ -1,12 +1,23 @@
-import 'package:flutter/material.dart';
 
+
+import 'package:flutter/material.dart';
 
 class Todo {
   final int id;
   final String title;
   final String description;
   final bool isCompleted;
-  const Todo({this.id,this.title,this.description,this.isCompleted});
+
+  const Todo({this.id, this.title, this.description, this.isCompleted});
+
+}
+
+class AppState {
+
+  final List<Todo> list;
+  final List<String> titles;
+  final List<GlobalKey> keys;
+  const AppState({this.list,this.titles,this.keys});
 
 }
 
